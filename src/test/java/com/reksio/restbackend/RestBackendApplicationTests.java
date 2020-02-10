987414ba4dcb053f5cf0291bf591269b7262a04c
mpusio <pusiomichal@gmail.com>
2020-02-10 10:28:31 +0100

    Init spring boot project

---
+++ src/test/java/com/reksio/restbackend/RestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reksio.restbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
